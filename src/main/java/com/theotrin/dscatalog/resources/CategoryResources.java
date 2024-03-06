0c98f3d842114fa338bb27d88aa4d14a145f0747
unknown <theopinheiro12@gmail.com>
2024-03-06 14:54:33 -0400

    POST /category added

--- src/main/java/com/theotrin/dscatalog/resources/CategoryResources.java
+++ src/main/java/com/theotrin/dscatalog/resources/CategoryResources.java
@@ -5,11 +5,10 @@ import com.theotrin.dscatalog.entities.Category;
 import com.theotrin.dscatalog.services.CategoryServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,16 +16,27 @@ import java.util.List;
 public class CategoryResources {
 
     @Autowired
-    private CategoryServices services;
+    private CategoryServices service;
     @GetMapping
     public ResponseEntity<List<CategoryDTO>> findAll(){
-        List<CategoryDTO> list = services.findAll();
+        List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     };
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
-        CategoryDTO dto = services.findById(id);
+        CategoryDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
     };
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+        dto = service.insert(dto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    };
+
 }
--- src/main/java/com/theotrin/dscatalog/services/CategoryServices.java
+++ src/main/java/com/theotrin/dscatalog/services/CategoryServices.java
@@ -33,6 +33,12 @@ import java.util.stream.Collectors;
 
             return new CategoryDTO(entity);
         }
-
+        @Transactional
+        public CategoryDTO insert(CategoryDTO dto) {
+            Category entity = new Category();
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+            return new CategoryDTO(entity);
+        }
     }
 
